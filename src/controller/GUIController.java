b213e3e8ef822a31caffd2c9cb3a2b5fd4c8f3ec
abosma <atillabosma@hotmail.com>
2017-12-13 15:31:16 +0100

    Panelcode in eigen functie voor mooiere code

--- src/controller/GUIController.java
+++ src/controller/GUIController.java
@@ -22,12 +22,6 @@ import javax.swing.border.BevelBorder;
 
 @SuppressWarnings("serial")
 public class GUIController extends javax.swing.JFrame implements ActionListener {
-
-	private JPanel jPanel1;
-	private JPanel pnlWagons;
-	private JTextField tfNewTrain;
-	private JPanel jPanel2;
-	private JPanel drawPanel;
 	
 	public GUIController() 
 	{
@@ -48,78 +42,63 @@ public class GUIController extends javax.swing.JFrame implements ActionListener 
 			thisLayout.columnWidths = new int[] {7, 7, 7, 7};
 			getContentPane().setLayout(thisLayout);
 			
-			jPanel1 = new JPanel();
-			jPanel1.setLayout(new BorderLayout());
-			getContentPane().add(jPanel1, new GridBagConstraints(0, 0, 4, 2, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+			JPanel mainPanel = new JPanel();
+			mainPanel.setLayout(new BorderLayout());
+			getContentPane().add(mainPanel, new GridBagConstraints(0, 0, 4, 2, 0.0, 0.0, GridBagConstraints.EAST, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
 			
-			drawPanel = new JPanel();
+			JPanel drawPanel = new JPanel();
 			drawPanel.setBackground(Color.WHITE);
-			jPanel1.add(drawPanel,BorderLayout.CENTER);
+			mainPanel.add(drawPanel,BorderLayout.CENTER);
 
-			jPanel2 = new JPanel();
-			GridBagLayout jPanel2Layout = new GridBagLayout();
-			jPanel2.setLayout(jPanel2Layout);
-			getContentPane().add(jPanel2, new GridBagConstraints(0, 2, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+			JPanel trainPanel = createJPanel(0, 2, 1, 1);
 			
 			JTextPane tpTextTrain = new JTextPane();
 			tpTextTrain.setText("Train name:");
 			tpTextTrain.setEditable(false);
+			trainPanel.add(tpTextTrain, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+			trainPanel.setBounds(10, 10, 100, 15);
 			
-			jPanel2.add(tpTextTrain, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-			jPanel2.setBounds(10, 10, 100, 15);
-			jPanel2Layout.rowWeights = new double[] {0.1, 0.1, 0.1, 0.1};
-			jPanel2Layout.rowHeights = new int[] {7, 7, 7, 7};
-			jPanel2Layout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1};
-			jPanel2Layout.columnWidths = new int[] {7, 7, 7, 7};
+			JTextField tfNewTrain = new JTextField(20);
+			trainPanel.add(tfNewTrain, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
 			
-			tfNewTrain = new JTextField(20);
-			jPanel2.add(tfNewTrain, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
-			
-			JButton createTrain = createButton(2, 0, 1, 1, jPanel2, "Create Train");
+			JButton createTrain = createButton(2, 0, 1, 1, trainPanel, "Create Train");
 			createTrain.addActionListener(a -> 
 				System.out.println("test")
 			);
 			
-			JComboBox cbAllTrains = createComboBox(1, 1, 1, 2, jPanel2);
+			JComboBox cbAllTrains = createComboBox(1, 1, 1, 2, trainPanel);
 			cbAllTrains.addActionListener(a -> 
 				System.out.println("Test")
 			);
 			
-			JButton deleteTrain = createButton(2, 2, 1, 1, jPanel2, "Delete Train");
+			JButton deleteTrain = createButton(2, 2, 1, 1, trainPanel, "Delete Train");
 			deleteTrain.addActionListener(a -> 
 				System.out.println("Test")
 			);
 
-			pnlWagons = new JPanel();
-			GridBagLayout jPanel3Layout = new GridBagLayout();
-			getContentPane().add(pnlWagons, new GridBagConstraints(1, 2, 2, 3, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
-			jPanel3Layout.rowWeights = new double[] {0.1, 0.1, 0.1, 0.1};
-			jPanel3Layout.rowHeights = new int[] {7, 7, 7, 7};
-			jPanel3Layout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1};
-			jPanel3Layout.columnWidths = new int[] {7, 7, 7, 7};
-			pnlWagons.setLayout(jPanel3Layout);
-			pnlWagons.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
+			JPanel wagonPanel = createJPanel(1, 2, 2, 3);
+			wagonPanel.setBorder(BorderFactory.createEtchedBorder(BevelBorder.LOWERED));
 			
 			JTextField tfNewWagon = new JTextField(20);
-			pnlWagons.add(tfNewWagon, new GridBagConstraints(1, 0, 1, 2, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
+			wagonPanel.add(tfNewWagon, new GridBagConstraints(1, 0, 1, 2, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0));
 			
 			JTextPane tfCreateWagon = new JTextPane();
-			pnlWagons.add(tfCreateWagon, new GridBagConstraints(0, 0, 1, 2, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
+			wagonPanel.add(tfCreateWagon, new GridBagConstraints(0, 0, 1, 2, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
 			tfCreateWagon.setText("Wagon Name: ");
 			tfCreateWagon.setEditable(false);
 			
-			JButton addWagon = createButton(1, 1, 1, 1, pnlWagons,"Add Wagon");
+			JButton addWagon = createButton(1, 1, 1, 1, wagonPanel,"Add Wagon");
 			addWagon.addActionListener(a -> 
 				System.out.println("Test")
 			);
 			
-			JComboBox cbAllWagons = createComboBox(1, 2, 1, 2, pnlWagons);
+			JComboBox cbAllWagons = createComboBox(1, 2, 1, 2, wagonPanel);
 			cbAllWagons.addActionListener(a -> 
 				System.out.println("Test")
 			);
 			
 			
-			JButton deleteWagon = createButton(1, 3, 1, 1, pnlWagons, "Delete Wagon");
+			JButton deleteWagon = createButton(1, 3, 1, 1, wagonPanel, "Delete Wagon");
 			deleteWagon.addActionListener(a -> 
 				System.out.println("Test")
 			);
@@ -146,6 +125,18 @@ public class GUIController extends javax.swing.JFrame implements ActionListener 
 		panel.add(cbTempComboBox, new GridBagConstraints(x, y, width, height, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(0, 0, 0, 0), 0, 0));
 		cbTempComboBox.setModel(cbTempModel);
 		return cbTempComboBox;
+	}
+	
+	private JPanel createJPanel(int x, int y, int width, int height) {
+		JPanel tempPanel = new JPanel();
+		GridBagLayout tempLayout = new GridBagLayout();
+		tempLayout.rowWeights = new double[] {0.1, 0.1, 0.1, 0.1};
+		tempLayout.rowHeights = new int[] {7, 7, 7, 7};
+		tempLayout.columnWeights = new double[] {0.1, 0.1, 0.1, 0.1};
+		tempLayout.columnWidths = new int[] {7, 7, 7, 7};
+		tempPanel.setLayout(tempLayout);
+		getContentPane().add(tempPanel, new GridBagConstraints(x, y, width, height, 0.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+		return tempPanel;
 	}
 	
 	@Override
