57c0edcc2307f7ffd0e18fa57beccc0585b5638b
ronaldderooy <ronnieflexican@gmail.com>
2017-12-14 16:06:54 +0100

    train image

--- src/Model/Train.java
+++ src/Model/Train.java
@@ -2,15 +2,21 @@ package Model;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.*;
+
+import javax.imageio.ImageIO;
 
 public class Train implements Serializable, IItem {
 
 	private int _id;
 	private String _name;
 	private ArrayList<IItem> allWagons = new ArrayList<IItem>();
-
+	private transient BufferedImage img;
 	public Train(String name, int id) {
 		this._name = name;
 		this._id = id;
@@ -38,14 +44,27 @@ public class Train implements Serializable, IItem {
 
 	@Override
 	public void draw(Graphics graphics) {
-		graphics.setColor(Color.LIGHT_GRAY);
-		graphics.fillRect(30, 80 + _id * 100, 80, 40);
-		graphics.fillRect(80, 60 + _id * 100, 30, 30);
-		graphics.drawRoundRect(85, 40 + _id * 100, 20, 20, 20, 20);
-		graphics.drawRoundRect(85, _id * 100, 40, 40, 40, 40);
-		graphics.setColor(Color.BLACK);
-		graphics.fillRoundRect(35, 120 + _id * 100, 20, 20, 20, 20);
-		graphics.fillRoundRect(80, 120 + _id * 100, 20, 20, 20, 20);
-		graphics.drawString(_name, 40, 105 + _id * 100);
+		
+		System.out.println(graphics);
+		
+	
+		try {
+			
+		    img = ImageIO.read(new File("C:\\Users\\ronald\\git\\PFPoorRail\\src\\images\\train.png"));
+		    System.out.println(img.getWidth());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		graphics.drawImage(img, -10,-10, null);
+//		graphics.setColor(Color.LIGHT_GRAY);
+//		graphics.fillRect(30, 80 + _id * 100, 80, 40);
+//		graphics.fillRect(80, 60 + _id * 100, 30, 30);
+//		graphics.drawRoundRect(85, 40 + _id * 100, 20, 20, 20, 20);
+//		graphics.drawRoundRect(85, _id * 100, 40, 40, 40, 40);
+//		graphics.setColor(Color.BLACK);
+//		graphics.fillRoundRect(35, 120 + _id * 100, 20, 20, 20, 20);
+//		graphics.fillRoundRect(80, 120 + _id * 100, 20, 20, 20, 20);
+//		graphics.drawString(_name, 40, 105 + _id * 100);
 	}
 }
